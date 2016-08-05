5f886c5dad60bd426cf4c35d72a0355e7a861ad8
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-08-05 10:37:30 +0100

    testAncestors added

--- src/test/java/org/reactome/web/pwp/model/client/GwtTestLists.java
+++ src/test/java/org/reactome/web/pwp/model/client/GwtTestLists.java
@@ -1,11 +1,12 @@
 package org.reactome.web.pwp.model.client;
 
-import org.reactome.web.pwp.model.client.classes.Pathway;
-import org.reactome.web.pwp.model.client.classes.Species;
+import org.reactome.web.pwp.model.client.classes.*;
 import org.reactome.web.pwp.model.client.common.ContentClientHandler;
 import org.reactome.web.pwp.model.client.common.GWTTestCaseCommon;
 import org.reactome.web.pwp.model.client.content.ContentClient;
 import org.reactome.web.pwp.model.client.content.ContentClientError;
+import org.reactome.web.pwp.model.client.util.Ancestors;
+import org.reactome.web.pwp.model.client.util.Path;
 
 import java.util.List;
 
@@ -39,7 +40,7 @@ public class GwtTestLists extends GWTTestCaseCommon {
         });
     }
 
-    public void tetsSpeciesList(){
+    public void testSpeciesList(){
         // Since RPC calls are asynchronous, we will need to wait for a response
         // after this test method returns. This line tells the test runner to wait
         // up to 2.5 seconds before timing out.
@@ -51,6 +52,49 @@ public class GwtTestLists extends GWTTestCaseCommon {
                 assertTrue("Species list cannot be empty", species.size() > 0);
                 assertTrue("The first element in the species list has to be human", species.get(0).getTaxId().equals("9606"));
                 finishTest();
+            }
+
+            @Override
+            public void onContentClientException(Type type, String message) {
+                fail(type + " " + message);
+            }
+
+            @Override
+            public void onContentClientError(ContentClientError error) {
+                fail(error.getMessage().toString());
+            }
+        });
+    }
+
+    public void testAncestors(){
+        // Since RPC calls are asynchronous, we will need to wait for a response
+        // after this test method returns. This line tells the test runner to wait
+        // up to 2.5 seconds before timing out.
+        delayTestFinish(2500);
+
+        ContentClient.query("R-HSA-5673001", new ContentClientHandler.ObjectReady() {
+            @Override
+            public void onObjectReady(DatabaseObject databaseObject) {
+                ContentClient.getAncestors((Event) databaseObject, new ContentClientHandler.AncestorsLoaded() {
+                    @Override
+                    public void onAncestorsLoaded(Ancestors ancestors) {
+                        assertTrue(ancestors.size()>0);
+                        Path path = ancestors.get(0);
+                        assertTrue(path.size()>0);
+                        assertTrue(path.get(path.size()-1) instanceof TopLevelPathway);
+                        finishTest();
+                    }
+
+                    @Override
+                    public void onContentClientException(Type type, String message) {
+                        fail(type + " " + message);
+                    }
+
+                    @Override
+                    public void onContentClientError(ContentClientError error) {
+                        fail(error.getMessage().toString());
+                    }
+                });
             }
 
             @Override
